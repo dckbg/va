d667f7d42fa79133d5bb79cb6dd4c16a72f38c30
dckbg <32584423+dckbg@users.noreply.github.com>
2017-10-12 07:24:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 cd /gsnova
+if [ "$SERVER_JSON" != none ]; then
+  echo -e "$SERVER_JSON" > server.json
+fi
+
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
   echo -e "$CERT_PEM" > cert.pem
   echo -e "$KEY_PEM"  > key.pem
